7721e30e9164f9f522684bbe28739d86279c81a5
Samuel <samuel.charlet62@gmail.com>
2022-10-24 15:01:32 +0200

    Commented Code

--- Assets/_Scripts/GroundManager.cs
+++ Assets/_Scripts/GroundManager.cs
@@ -12,41 +12,42 @@ public class GroundManager : MonoBehaviour
     [SerializeField] private Material[] _groundMats;
     [SerializeField] private GameObject _indicator;
 
-    private float _startYPos;
     private bool _isEntered;
-    public bool IsSelected;
+    [HideInInspector] public bool IsSelected; //It's public for a security test
     private bool _isArounded;
     private Material _mat;
 
 
     private void Start()
     {
-        _startYPos = transform.position.y;
         _mat = _indicator.GetComponent<MeshRenderer>().material;
-        OnLeaved(true);
+        ResetMat();
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        //detect if the trigger has entered
         if (other.gameObject.GetComponentInParent<FollowMouse>())
             OnEntered();
     }
 
     private void OnTriggerExit(Collider other)
     {
+        //detect if the trigger has quit
         if (other.gameObject.GetComponentInParent<FollowMouse>())
-            OnLeaved(false);
+            OnLeaved();
     }
 
     public void ChangeCoords(Vector2Int newCoords)
     {
         GroundCoords = newCoords;
-    }
+    } //Change the coords of the ground
 
     private void OnEntered()
     {
+        //Prevent to change the mat if its actually selected
         if (IsSelected) return;
-
+        //Change mat and _isEntered
         _indicator.GetComponent<MeshRenderer>().material = _groundMats[0];
         _isEntered = true;
     }
@@ -58,31 +59,31 @@ public class GroundManager : MonoBehaviour
         IsSelected = false;
         _indicator.GetComponent<MeshRenderer>().material = _mat;
     }
-    public void OnLeaved(bool hasForced)
+    private void OnLeaved()
     {
-        if (hasForced) IsSelected = false;
-
+        //Prevent to change the mat if its actually selected
         if (IsSelected) return;
-
+        //Put the aroundedMat if it was arounded else base mat
         _indicator.GetComponent<MeshRenderer>().material = _isArounded ? _groundMats[1] : _mat;
-
+        //Reset _isEntered
         _isEntered = false;
     }
 
     // ReSharper disable Unity.PerformanceAnalysis
     private void OnSelected()
     {
+        //Don't need to recall this function if its actually selected
         if (IsSelected) return;
-        
+        //Change the mat and call CheckIfSelected in the MapManager to swap or not        
         IsSelected = true;
         _indicator.GetComponent<MeshRenderer>().material = _groundMats[2];
         MapManager.Instance.CheckIfSelected(gameObject, GroundCoords);
-        // print("OnSelected");
     }
 
     public void OnArounded()
     {
         _isArounded = true;
+        //Security if not already entered or selected
         if (_isArounded && !_isEntered && !IsSelected)
             _indicator.GetComponent<MeshRenderer>().material = _groundMats[1];
     }
--- Assets/_Scripts/MapManager.cs
+++ Assets/_Scripts/MapManager.cs
@@ -12,9 +12,7 @@ public class MapManager : MonoBehaviour
 {
     [SerializeField] private GameObject _map = null;
     [SerializeField] private GameObject[] _environment = null;
-
     [SerializeField] private float _distance = 0;
-    // [SerializeField] private GameObject _triggerAroundSelected = null;
 
     private string[] _mapInfo;
     private Vector2Int _mapSize;
@@ -41,72 +39,86 @@ public class MapManager : MonoBehaviour
 
     private void Start()
     {
+        //Get the text map
         string map = Application.dataPath + "/Map-DontMove/Map.txt";
         _mapInfo = File.ReadAllLines(map);
+        //Get its size
         _mapSize.x = _mapInfo[0].Length;
         _mapSize.y = _mapInfo.Length;
-
+        //Init the grids
         _mapGrid = new GameObject[_mapSize.x, _mapSize.y];
         _tempGrid = new int[_mapSize.x, _mapSize.y];
-
+        //Init the offset
         _mapOffset.x = _distance * _mapSize.x * .5f - (_distance * .5f);
         _mapOffset.y = _distance * _mapSize.y - (_distance * .5f);
+        //Start init
         InitializeLevel(_mapSize);
+        //Reset the lastSelected and tempGrid
         ResetLastSelected();
         ResetTempGrid();
     }
 
-    private void InitializeLevel(Vector2Int sizeMap)
+    private void InitializeLevel(Vector2Int sizeMap) //Map creation
     {
         for (int i = 0; i < sizeMap.x; i++)
         {
             for (int j = 0; j < sizeMap.y; j++)
             {
+                //Get the string of the actual line
                 string line = _mapInfo[i];
+                //Get the actual char of the string of the actual line
                 char whichEnvironment = line[j];
 
                 switch (whichEnvironment)
                 {
                     case GROUND_WHITE:
-                        GameObject go = Instantiate(_environment[0], _map.transform);
+                        //Instantiate the good ground into the map parent
+                        GameObject go = Instantiate(_environment[0], _map.transform); 
+                        //Tp ground to its position
                         go.transform.position = new Vector3(j * _distance - _mapOffset.x, 0,
-                            sizeMap.x - 1 - i * _distance - _mapOffset.y);
+                            sizeMap.x - 1 - i * _distance - _mapOffset.y); 
+                        //Change coords of the ground
+                        go.GetComponent<GroundManager>().ChangeCoords(new Vector2Int(j, sizeMap.x - 1 - i)); 
+                        //Update _mapGrid
+                        _mapGrid[j, sizeMap.x - 1 - i] = go; 
+                        //Spawn anim
                         MapSpawnAnim(go);
-                        _mapGrid[j, sizeMap.x - 1 - i] = go;
-                        go.GetComponent<GroundManager>().ChangeCoords(new Vector2Int(j, sizeMap.x - 1 - i));
                         break;
 
                     case GROUND_GREY:
                         GameObject go1 = Instantiate(_environment[1], _map.transform);
                         go1.transform.position = new Vector3(j * _distance - _mapOffset.x, 0,
                             sizeMap.x - 1 - i * _distance - _mapOffset.y);
-                        MapSpawnAnim(go1);
-                        _mapGrid[j, sizeMap.x - 1 - i] = go1;
                         go1.GetComponent<GroundManager>().ChangeCoords(new Vector2Int(j, sizeMap.x - 1 - i));
+                        
+                        _mapGrid[j, sizeMap.x - 1 - i] = go1;
+                        MapSpawnAnim(go1);
                         break;
                     case GROUND_HARD:
                         GameObject go2 = Instantiate(_environment[2], _map.transform);
                         go2.transform.position = new Vector3(j * _distance - _mapOffset.x, 0,
                             sizeMap.x - 1 - i * _distance - _mapOffset.y);
-                        MapSpawnAnim(go2);
+                        
                         _mapGrid[j, sizeMap.x - 1 - i] = go2;
+                        MapSpawnAnim(go2);
                         break;
                 }
             }
         }
-
+        //Rotate the map for the isometric view / need to do it after the creation or change the InitializeLevel
         _map.transform.Rotate(new Vector3(0, 45, 0));
     }
 
     private void MapSpawnAnim(GameObject which)
     {
+        //Funny ground animation spawn
         which.transform.DOScale(Vector3.zero, 0f);
         which.transform.DOScale(Vector3.one, .5f);
     }
 
     public void CheckIfSelected(GameObject which, Vector2Int newCoords)
     {
-        //if was check around -> go swap
+        //If was checkAround -> go swap
         if (_tempGrid[newCoords.x, newCoords.y] == 1)
             Swap(which, newCoords);
         else
@@ -118,61 +130,67 @@ public class MapManager : MonoBehaviour
         //update map
         _mapGrid[newCoords.x, newCoords.y] = _lastBlocSelected;
         _mapGrid[_lastCoordsSelected.x, _lastCoordsSelected.y] = which;
-        //change position
+        //Change position
         (_lastBlocSelected.transform.position, which.transform.position) = (which.transform.position, _lastBlocSelected.transform.position);
-        //change coords inside of GroundManager
+        //Change coords inside of GroundManager
         _lastBlocSelected.GetComponent<GroundManager>().GroundCoords = newCoords;
         which.GetComponent<GroundManager>().GroundCoords = _lastCoordsSelected;
-        //reset selection's color of the two Grounds
+        //Reset selection's color of the two Grounds
         _lastBlocSelected.GetComponent<GroundManager>().ResetMat();
         which.GetComponent<GroundManager>().ResetMat();
-        //reset selection
+        //Reset selection
         ResetTempGrid();
         ResetLastSelected();
     }
 
     private void CheckAround(GameObject which, Vector2Int coords)
     {
+        //Reset to start from scratch
         ResetLastSelected();
         ResetTempGrid();
+        //Update lastSelected if need to call Swap() after
         _lastBlocSelected = which;
         _lastCoordsSelected = coords;
         foreach (var dir in _directions)
         {
             Vector2Int newPos = new Vector2Int(coords.x + dir.x, coords.y + dir.y);
-            //check if inside of array
+            //Check if inside of array
             if (newPos.x < 0 || newPos.x >= _mapSize.x || newPos.y < 0 || newPos.y >= _mapSize.y) continue;
-            //check if something exist
+            //Check if something exist
             if (_mapGrid[newPos.x, newPos.y] == null) continue;
-            //check if has GroundManager
+            //Check if has GroundManager
             if (!_mapGrid[newPos.x, newPos.y].GetComponent<GroundManager>()) continue;
-            //check if ground CanBeMoved
+            //Check if ground CanBeMoved
             if (!_mapGrid[newPos.x, newPos.y].GetComponent<GroundManager>().CanBeMoved) continue;
-            //check if not actually selected -> Security
+            //Check if not actually selected -> Security
             if (_mapGrid[newPos.x, newPos.y].GetComponent<GroundManager>().IsSelected) continue;
-            //it's okay
+            //It's good
             _mapGrid[newPos.x, newPos.y].GetComponent<GroundManager>().OnArounded();
             _tempGrid[newPos.x, newPos.y] = 1;
             _groundArounded.Add(_mapGrid[newPos.x, newPos.y]);
         }
     }
 
+    private const int BIG_COORDS = 5000;
     private void ResetLastSelected()
     {
+        //Reset mat
         if(_lastBlocSelected != null)
             _lastBlocSelected.GetComponent<GroundManager>().ResetMat();
-            
+        //Reset lastSelected and put big big coords to avoid problem with x = 0 or y =0
         _lastBlocSelected = null;
-        _lastCoordsSelected = Vector2Int.one * 500;
-
+        _lastCoordsSelected = Vector2Int.one * BIG_COORDS;
+        //Reset mat of all ground selected before
         foreach (var ground in _groundArounded)
         {
             ground.GetComponent<GroundManager>().ResetMat();
         }
+        //Clear the _groundArounded List
         _groundArounded.Clear();
     }
     private void ResetTempGrid()
     {
+        //Reset the tempGrid for the ground arounded
         for (int x = 0; x < _mapSize.x; x++)
         {
             for (int y = 0; y < _mapSize.y; y++)
