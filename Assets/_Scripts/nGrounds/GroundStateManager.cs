9fe469fe1f65a36c9a31fec06b2b06d24691f5d5
Samuel <samuel.charlet62@gmail.com>
2022-12-28 12:16:32 +0100

    Delete unusable using

--- Assets/_Scripts/nGrounds/GroundStateManager.cs
+++ Assets/_Scripts/nGrounds/GroundStateManager.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using Unity.Android.Types;
 using UnityEngine;
 
 public class GroundStateManager : MonoBehaviour
