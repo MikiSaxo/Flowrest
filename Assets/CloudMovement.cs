b34af90a6e279e033bac4bb985cddf375d26c87d
Manuel Antonio <antonio.manuel.private@gmail.com>
2023-05-15 16:20:43 +0200

    Mesh Particule Manager

--- Assets/CloudMovement.cs
+++ Assets/CloudMovement.cs
@@ -8,11 +8,15 @@ public class CloudMovement : MonoBehaviour
 {
     public void InitMovement(Transform endPos, float timeToEnd)
     {
-        gameObject.transform.DOMoveX(endPos.position.x, timeToEnd).OnComplete(DestroyCloud).SetEase(Ease.Linear);
+        // J'utilise dotween pour modifier SEULEMENT la position en X avec le "DOMoveX"
+        // Je lui donne le comportement Ease.Linear car je veux qu'il se déplace de manière linéaire
+        // Quand il a fini de ce déplacer, avec le "OnComplete", j'appelle la fonction "DestroyCloud" pour le détruire 
+        gameObject.transform.DOMoveX(endPos.position.x, timeToEnd).SetEase(Ease.Linear).OnComplete(DestroyCloud);
     }
 
     private void DestroyCloud()
     {
+        // Je détruis le nuage 
         Destroy(gameObject);
     }
 }
